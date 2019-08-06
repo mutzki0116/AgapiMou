f10ce0ee6ed3de89fdfc8de080e8d4224da0c33b
Escanor <escanor0116@gmail.com>
2019-08-06 14:35:40 +0800

    Template Blending Trial 1

---
+++ database/migrations/2019_08_01_081946_create_assignments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAssignmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assignments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('a_category');
+            $table->string('a_name');
+            $table->string('a_description');
+            $table->string('a_photo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assignments');
+    }
+}
